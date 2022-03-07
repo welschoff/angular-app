c1e5a14ea21b753d6d7312e18bd049c210df012a
Welschoff <h.welschoff@gmx.de>
2022-03-07 15:52:36 +0100

    ngClass and ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,20 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
   constructor() {
-    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
   getServerStatus() {
@@ -17,7 +24,7 @@ export class ServerComponent {
   }
 
   getColor() {
-    return this.serverStatus === 'online' ? 'red' : 'red';
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
 
