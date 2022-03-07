9b5771b2dd0aeb6d274e9000bd76cf9a649f39a6
Welschoff <h.welschoff@gmx.de>
2022-03-07 16:39:15 +0100

    Excercise: Practicing Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles: [
-    `
-      h3 {
-        color: darkblue;
-      }
-    `,
-  ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '';
   username = '';
+  displayed = false;
+  log = [];
+
+  onToggleDetails() {
+    this.displayed = !this.displayed;
+    this.log.push(this.log.length + 1);
+  }
 }
